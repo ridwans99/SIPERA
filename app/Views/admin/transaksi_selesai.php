8a51b22dfe4bfad6f85e3cba38c50d1259aa92a3
Ridwan Syah <56246049+ridwans99@users.noreply.github.com>
2020-12-08 09:36:00 +0700

    Update transaksi_selesai.php

--- app/Views/admin/transaksi_selesai.php
+++ app/Views/admin/transaksi_selesai.php
@@ -6,45 +6,39 @@
           <div class="card-header">
             <div class="card-title"><i class="fas fa-retweet text-danger"></i> Transaksi Selesai</div> 
           </div>
-          <?php foreach ($peminjaman as $p) { ?>
+          <?php foreach ($peminjaman_r as $pr) { ?>
             <form action="<?php echo base_url().'admin/transaksi_selesai_act' ?>" method="post">
-              <input type="hidden" name="id" value="<?php echo $p->id_pinjam ?>">
-              <input type="hidden" name="buku" value="<?php echo $p->id_buku ?>">
-              <input type="hidden" name="tgl_kembali" value="<?php echo $p->tgl_kembali ?>">
-              <input type="hidden" name="Denda" value="<?php echo $p->denda ?>">
+              <input type="hidden" name="id" value="<?php echo $pr->id_pinjam ?>">
+              <input type="hidden" name="ruangan" value="<?php echo $pr->id_ruangan ?>">
+              <input type="hidden" name="tgl_kembali" value="<?php echo $pr->tgl_kembali ?>">
               <div class="form-group">
                 <label><i class="fas fa-user text-danger"></i> Nama Anggota</label>
                 <select class="form-control" name="anggota" disabled>
                   <option value="">-Pilih Anggota-</option>
                   <?php foreach ($anggota as $k) { ?>
-                    <option <?php if($p->id_anggota==$k->id_anggota){echo "selected='selected'";} ?> value="<?php echo $k->id_anggota; ?>"><?php echo $k->nama_anggota; ?></option>
+                    <option <?php if($pr->id_anggota==$k->id_anggota){echo "selected='selected'";} ?> value="<?php echo $k->id_anggota; ?>"><?php echo $k->nama_anggota; ?></option>
                   <?php } ?>
                 </select>
               </div>
 
               <div class="form-group">
-                <label><i class="fas fa-book text-danger"></i> Buku</label>
-                <select class="form-control" name="buku" disabled>
-                  <option value="">-Pilih Buku-</option>
-                  <?php foreach ($buku as $m) { ?>
-                    <option <?php if($p->id_buku==$m->id_buku){echo "selected='selected'";} ?> value="<?php echo $m->id_buku; ?>"><?php echo $m->judul_buku; ?></option>
+                <label><i class="fas fa-book text-danger"></i> Ruangan</label>
+                <select class="form-control" name="ruangan" disabled>
+                  <option value="">-Pilih Ruangan-</option>
+                  <?php foreach ($ruangan as $m) { ?>
+                    <option <?php if($pr->id_ruangan==$m->id_ruangan){echo "selected='selected'";} ?> value="<?php echo $m->id_ruangan; ?>"><?php echo $m->ruangan; ?></option>
                   <?php } ?>
                 </select>
               </div>
 
               <div class="form-group">
                 <label><i class="fas fa-calendar-alt text-danger"></i> Tanggal Pinjam</label>
-                <input class="form-control" type="date" name="tgl_pinjam" value="<?php echo $p->tgl_pinjam ?>" disabled>
+                <input class="form-control" type="date" name="tgl_pinjam" value="<?php echo $pr->tgl_pinjam ?>" disabled>
               </div>
 
               <div class="form-group">
                 <label><i class="fas fa-calendar-alt text-danger"></i> Tanggal Kembali</label>
-                <input class="form-control" type="date" name="tgl_kembali" value="<?php echo $p->tgl_kembali ?>" disabled>
-              </div>
-
-              <div class="form-group">
-                <label><i class="fas fa-money-bill text-danger"></i> Denda/Hari</label>
-                <input class="form-control" type="text" name="denda" value="<?php echo $p->denda ?>" disabled>
+                <input class="form-control" type="date" name="tgl_kembali" value="<?php echo $pr->tgl_kembali ?>" disabled>
               </div>
 
               <div class="form-group">
