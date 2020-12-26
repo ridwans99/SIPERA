94bccd9555a4c1dd6af850f96cfe856ace343790
Ridwan Syah <56246049+ridwans99@users.noreply.github.com>
2020-12-26 15:55:33 +0700

    Add files via upload

---
+++ app/Models/TransaksiBarangModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TransaksiBarangModel extends Model
+{
+    // protected $table = 'users';
+    // protected $primaryKey = 'user_id';
+    // protected $useTimestamps = true;
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+    }
+
+    public function tampildata()
+    {
+        return $this->db->table('ordersbarang')->get();
+    }
+}
