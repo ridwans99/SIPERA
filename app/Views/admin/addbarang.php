16a80aa9aff978603fcb21bfea68b43d8754d177
Ridwan Syah <56246049+ridwans99@users.noreply.github.com>
2020-12-29 00:01:45 +0700

    Update addbarang.php

--- app/Views/admin/addbarang.php
+++ app/Views/admin/addbarang.php
@@ -6,23 +6,23 @@
         <div class="section-body">
             <div class="card">
                 <div class="card-body">
-                    <form action="/admin/addbarang" method="post" class="btn btn-primary" enctype="multipart/form-data">
+                    <form action="/Admin/insertBarang" method="post" class="btn btn-primary" enctype="multipart/form-data">
                         <div class="form-group">
                             <label>Nama Barang</label>
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="" name="nama">
+                                <input type="text" class="form-control" placeholder="" name="nama_barang" size="50">
                             </div>
                         </div>
                         <div class="form-group">
                             <label>Stock</label>
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="" name="stock">
+                                <input type="text" class="form-control" placeholder="" name="stock" size="50">
                             </div>
                         </div>
                         <div class="form-group">
                             <label>Status</label>
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="" name="status">
+                                <input type="text" class="form-control" placeholder="" name="status" size="50">
                             </div>
                         </div>
                         <input class="btn btn-primary btn-user btn-block" name='submit' type="submit" value='Submit' />
