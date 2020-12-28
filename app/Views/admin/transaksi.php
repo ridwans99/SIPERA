05bbab87b6e66b2fc7b4c04646355fea096fa8bf
Ridwan Syah <56246049+ridwans99@users.noreply.github.com>
2020-12-29 00:23:35 +0700

    Delete transaksi.php

--- app/Views/admin/transaksi.php
+++
@@ -1,8 +0,0 @@
-<!-- Main Content -->
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Daftar Transaksi</h1>
-        </div>
-    </section>
-</div>
