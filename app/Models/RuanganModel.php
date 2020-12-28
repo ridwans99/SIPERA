d48b4a4140a336b0123d6e9fdf2b3603d245d0bc
Ridwan Syah <56246049+ridwans99@users.noreply.github.com>
2020-12-29 00:52:30 +0700

    Update RuanganModel.php

--- app/Models/RuanganModel.php
+++ app/Models/RuanganModel.php
@@ -6,17 +6,33 @@ use CodeIgniter\Model;
 
 class RuanganModel extends Model
 {
-    // protected $table = 'users';
-    // protected $primaryKey = 'user_id';
-    // protected $useTimestamps = true;
+    protected $table = 'ruangans';
+    protected $primaryKey = 'ruangan_id';
+    protected $useTimestamps = true;
 
-    public function __construct()
+    public function tampildata($id = false)
     {
-        $this->db = db_connect();
+        if ($id) {
+            return $this->where(['ruangan_id' => $id])->first();
+        } else {
+            return $this->findAll();
+        }
     }
 
-    public function tampildata()
+    public function tambah($data)
     {
-        return $this->db->table('ruangans')->get();
+        return $this->db->table('ruangans')->insert($data);
+    }
+
+    public function updateRuangan($data, $id)
+    {
+        $query = $this->db->table($this->table)->update($data, array('ruangan_id' => $id));
+        return $query;
+    }
+
+    public function deleteRuangan($id)
+    {
+        $query = $this->db->table($this->table)->delete(array('ruangan_id' => $id));
+        return $query;
     }
 }
