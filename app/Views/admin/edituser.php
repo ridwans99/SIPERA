3843f87a62e37bedbff29e2ed6b9bf74d54a6f13
Ridwan Syah <56246049+ridwans99@users.noreply.github.com>
2021-01-02 08:45:38 +0700

    Update edituser.php

--- app/Views/admin/edituser.php
+++ app/Views/admin/edituser.php
@@ -42,15 +42,22 @@
                         </div>
                         <div class="form-group">
                             <label>Prodi</label>
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control" value="<?= $tampildata['prodi']; ?>" placeholder="" name="prodi">
-                            </div>
+                            <select name="prodi" class="custom-select">
+                                <option <?php echo $tampildata['prodi'] == "Ilmu Komputer" ? "selected" : "" ?> value="Ilmu Komputer">Ilmu Komputer</option>
+                                <option <?php echo $tampildata['prodi'] == "Matematika" ? "selected" : "" ?> value="Matematika">Matematika</option>
+                                <option <?php echo $tampildata['prodi'] == "Pendidikan Matematika" ? "selected" : "" ?> value="Pendidikan Matematika">Pendidikan Matematika</option>
+                                <option <?php echo $tampildata['prodi'] == "Statistika" ? "selected" : "" ?> value="Statistika">Statistika</option>
+                            </select>
                         </div>
                         <div class="form-group">
                             <label>Angkatan</label>
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control" value="<?= $tampildata['angkatan']; ?>" placeholder="" name="angkatan">
-                            </div>
+                            <select name="angkatan" class="custom-select">
+                                <option <?php echo $tampildata['angkatan'] == "2016" ? "selected" : "" ?> value="2016">2016</option>
+                                <option <?php echo $tampildata['angkatan'] == "2017" ? "selected" : "" ?> value="2017">2017</option>
+                                <option <?php echo $tampildata['angkatan'] == "2018" ? "selected" : "" ?> value="2018">2018</option>
+                                <option <?php echo $tampildata['angkatan'] == "2019" ? "selected" : "" ?> value="2019">2019</option>
+                                <option <?php echo $tampildata['angkatan'] == "2020" ? "selected" : "" ?> value="2020">2020</option>
+                            </select>
                         </div>
                         <input class="btn btn-primary btn-user btn-block" name='submit' type="submit" value='Submit' />
                     </form>
