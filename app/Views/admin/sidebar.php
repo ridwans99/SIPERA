1a2c5619a45df91d36618980b978ef3a48a55468
Ridwan Syah <ridwansyah899@gmail.com>
2021-01-06 09:37:19 +0700

    logout

--- app/Views/admin/sidebar.php
+++ app/Views/admin/sidebar.php
@@ -13,12 +13,8 @@
                         <div class="d-sm-none d-lg-inline-block">Hi, Admin</div>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
-                        <div class="dropdown-title">Logged in 5 min ago</div>
-                        <a href="features-profile.html" class="dropdown-item has-icon">
-                            <i class="far fa-user"></i> Profile
-                        </a>
                         <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item has-icon text-danger">
+                        <a href="<?= base_url('Admin/logout') ?>" class="dropdown-item has-icon text-danger">
                             <i class="fas fa-sign-out-alt"></i> Logout
                         </a>
                     </div>
